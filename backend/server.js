667cce69a595e252f999e50c92b206dcbf51fdeb
ORUGANTIVINODKUMAR <144659708+ORUGANTIVINODKUMAR@users.noreply.github.com>
2026-05-22 18:34:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve frontend build
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
